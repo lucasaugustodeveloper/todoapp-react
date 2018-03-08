4384a9eada76d673f5fa7e324cdea89ea40309b5
Lucas Augusto <lucas.augusto5061@gmail.com>
2018-03-08 18:13:58 -0300

    terminado de fazer a API de todo

---
+++ backend/src/config/cors.js
@@ -0,0 +1,8 @@
+
+module.exports = function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-Width, Content-Type, Accept')
+
+  next()
+}
