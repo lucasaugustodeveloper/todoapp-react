797675658356bef773da327e17d55df5d74a8d08
Lucas Augusto <lucas.augusto5061@gmail.com>
2018-03-16 15:57:41 -0300

    adicionado componentes de PageHeader, todoForm é todoList

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -1,12 +1,20 @@
 import React, { Component } from 'react'
 
 import PageHeader from '../templates/pageHeader'
+import TodoForm from './todoForm'
+import TodoList from './todoList'
 
 class Todo extends Component {
   render () {
     return (
       <div>
-        <PageHeader name='Todo' small='list' />
+        <PageHeader name='Todo' small='cadastro' />
+        <div className='row'>
+          <TodoForm />
+        </div>
+        <div className='row'>
+          <TodoList />
+        </div>
       </div>
     )
   }
