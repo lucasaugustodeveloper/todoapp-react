9ca90b582dce0c1c940acfdc829de57477e0b248
Lucas Augusto <lucas.augusto5061@gmail.com>
2018-03-16 16:39:59 -0300

    adicionado prop para adicioanr mas classes css

--- frontend/src/templates/button/button.jsx
+++ frontend/src/templates/button/button.jsx
@@ -3,7 +3,7 @@ import If from '../../Helps/If'
 
 export default props => (
   <If test={!props.hide}>
-    <button className={`btn btn-${props.style} ${props.css}`} onClick={props.onCLick}>
+    <button className={`btn btn-${props.style} ${props.css}`} onClick={props.onClick}>
       <i className={`fa fa-${props.icon}`}></i>
     </button>
   </If>
